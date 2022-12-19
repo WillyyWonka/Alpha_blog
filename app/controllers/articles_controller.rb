9c47760753c14f485288f7e7669648f5dbd3579e
Ramiro Wittmann <wittmann.rg@gmail.com>
2022-12-19 10:22:48 -0300

    Con New implementado.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,4 +4,25 @@ class ArticlesController < ApplicationController
         @article = Article.find(params[:id])
     end
 
+    def index
+        @articles = Article.all
+    end
+
+    def new
+        @article = Article.new
+    end
+
+    def create
+        #render plain: params[:article]
+        @article = Article.new(params.require(:article).permit(:title, :description))
+       ## @article = Article.new(params[:title,:description])
+        #render plan: @article
+        if @article.save
+            flash[:notice] = "El articulo fue creado con exito!"
+            redirect_to article_path(@article)
+        else
+            render 'new'
+        end
+    end
+
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ApplicationRecord
-    validates :title, presence: true, length: {minimun: 10, maximum: 100}
-    validates :description, presence: true, length: {minimun: 30, maximum: 250}
+    validates :title, presence: true, length: {in: 10..100}
+    validates :description, presence: true, length: {in: 30..250}
 end
