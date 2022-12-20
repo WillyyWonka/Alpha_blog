455f5d11053f03e4c03ff914a637ac8f337f9337
Ramiro Wittmann <wittmann.rg@gmail.com>
2022-12-20 15:19:42 -0300

    Despues de un DRY (Do not repeat yourself)

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,8 @@
 class ArticlesController < ApplicationController
 
+before_action :set_article, only: [:show,:edit,:update,:destroy]
+
     def show
-        @article = Article.find(params[:id])
     end
 
     def index
@@ -13,10 +14,7 @@ class ArticlesController < ApplicationController
     end
 
     def create
-        #render plain: params[:article]
-        @article = Article.new(params.require(:article).permit(:title, :description))
-       ## @article = Article.new(params[:title,:description])
-        #render plan: @article
+        @article = Article.new(article_params)
         if @article.save
             flash[:notice] = "El articulo fue creado con exito!"
             redirect_to article_path(@article)
@@ -26,12 +24,10 @@ class ArticlesController < ApplicationController
     end
 
     def edit
-        @article = Article.find(params[:id])
     end
 
     def update
-        @article = Article.find(params[:id])
-        if @article.update(params.require(:article).permit(:title, :description))
+        if @article.update(article_params)
             flash[:notice]= "El articulo fue editado con exito!"
             redirect_to @article
         else
@@ -40,11 +36,19 @@ class ArticlesController < ApplicationController
     end
     
     def destroy
-        @article = Article.find(params[:id])
         @article.destroy
         flash[:success] = 'El articulo fue borrado con exito!'
-        redirect_to articles_path
-        
+        redirect_to articles_path 
+    end
+
+    private
+
+    def set_article
+        @article = Article.find(params[:id])
+    end
+
+    def article_params
+        params.require(:article).permit(:title, :description)
     end
 
 end
